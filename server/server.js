ae99f5f272bf37de1267b9270d4130247e5e6ad8
haseebmir07 <haseebrecovery393@gmail.com>
2025-12-01 19:40:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://theholidaycreators.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
